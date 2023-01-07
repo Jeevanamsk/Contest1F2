7f78af6e0b4fad0e67b463e5552d722a122c8bb4
Jeevana Mantina <113972063+Jeevanamsk@users.noreply.github.com>
2023-01-07 15:20:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-/** @format */
-
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
-
-function PrintDeveloperswithMap() {
-  //Write your code here
-}
-
-function PrintDeveloperbyForEach() {
-  //Write your code here
-}
-
-function addData() {
-  //Write your code here
-}
-
-function removeAdmin() {
-  //Write your code here
-}
-
-function concatenateArray() {
-  //Write your code here
-}
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  function PrintDeveloperswithMap() {
+    const professions = arr.map(employee => {
+        if (employee.profession === 'developer') {
+            return employee.profession;
+        }
+    });
+    console.log(professions);
+  }
+  
+  function PrintDeveloperbyForEach(){
+    arr.forEach(employee => {
+        if (employee.profession === 'developer') {
+          console.log(employee);
+        }
+        
+      });
+    
+  }
+  
+  function addData() {
+    const newEmployee = {id:4,name:"susan",age:"20",profession:"intern"};
+    arr.push(newEmployee);
+    console.log(arr);
+  }
+  
+  function removeAdmin() {
+    const updatedEmployees = arr.filter(employee => employee.profession !== 'admin');
+    console.log(updatedEmployees);
+  }
+  
+  function concatenateArray() {
+    const newData = [{id:5,name:"Jeevana",age:"20",profession:"designer"},
+                    {id:5,name:"Meghana",age:"22",profession:"developer"},
+                    {id:5,name:"jeevana",age:"20",profession:"tester"}];
+    const combinedData = arr.concat(newData);
+    console.log(combinedData);
+  }
+  
